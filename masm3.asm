86cd091285d04686f5a2c86739c6b5fd3712b63a
Typo Kign <typokign1@gmail.com>
2018-04-17 11:39:07 -0700

    Set strings menu

--- masm3.asm
+++ masm3.asm
@@ -1,8 +1,8 @@
 ;*****************************************************************************
-;Name:		David Cruz
+;Name:		David Cruz & Neal Hitzfield
 ;Program:	MASM3.asm
 ;Class:		CS3B
-;Lab:		MASM2
+;Lab:		MASM3
 ;Date:		April 19, 2018 at 11:59 PM
 ;Purpose:
 ;	Use the String1 and String2 libraries
@@ -17,6 +17,8 @@ ExitProcess PROTO Near32 stdcall, dVal:dword
 putstring 	PROTO Near32 stdcall, lpStringToPrint:dword
 intasc32	proto Near32 stdcall, lpStringToHold:dword, dval:dword
 hexToChar PROTO Near32 stdcall,lpDestStr:dword,lpSourceStr:dword,dLen:dword
+getstring	PROTO Near32 stdcall, lpStringToGet:dword, dlength:dword
+ascint32	PROTO Near32 stdcall, lpStringToConvert:dword  
 
 extern String_equals: Near32, String_equalsIgnoreCase: Near32,
 	   String_copy: Near32, String_substring_1: Near32, String_substring_2: Near32,
@@ -24,6 +26,7 @@ extern String_equals: Near32, String_equalsIgnoreCase: Near32,
 	   String_endsWith: Near32
 
 	.data
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; MENU ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 strMenuPrompt1		byte	"************************************************************",13,10,0
 strMenuPrompt2		byte	"*                          MASM 3                          *",13,10,0
 strMenuPrompt3		byte	"*----------------------------------------------------------*",13,10,0
@@ -52,15 +55,25 @@ strMenuPrompt25		byte	"* <22> String_toUpperCase                         current
 strMenuPrompt26		byte	"* <23> Quit                                                *",13,10,0
 strMenuPrompt27		byte	"************************************************************",13,10,0
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; INPUT ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+strChoicePrompt		byte	"Choice (1-23): ",0
+strChoice			byte	3 dup (?)
+dChoice				dword	0
+strInputPrompt1		byte	"Enter a value for String 1: ", 0
+strInputPrompt2		byte	"Enter a value for String 2: ", 0
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; FORMATTING ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 strCrlf				byte	13,10,0
 strTrue				byte	"TRUE",0
 strFalse			byte	"FALSE",0
 strNull				byte	"NULL",0
 strCurrently		byte	" currently:",0
 
-string1				byte	"asdfNULL", 100 dup (0)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; INPUT STRINGS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+string1				byte	"NULL", 100 dup (0)
 string2				byte	"NULL", 100 dup (0)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; MENU FUNCTION VALUES ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 dLength				dword	0
 strLength			byte	11 dup (?)
 strEquals			byte	"FALSE",0
@@ -236,12 +249,79 @@ Menu proc
 	invoke putstring, addr strCrlf
 	invoke putstring, addr strMenuPrompt26
 	invoke putstring, addr strMenuPrompt27
+	invoke putstring, addr strCrlf
 	ret
 Menu endp
 
+Input proc
+	invoke putstring, addr strChoicePrompt
+	invoke getstring, addr strChoice, 2
+	invoke putstring, addr strCrlf
+	invoke ascint32, addr strChoice
+	mov dChoice, eax
+
+	.if dChoice == 1
+		invoke putstring, addr strInputPrompt1
+		invoke getstring, addr string1, 100
+		invoke putstring, addr strCrlf
+	.elseif dChoice == 2
+		invoke putstring, addr strInputPrompt2
+		invoke getstring, addr string2, 100
+		invoke putstring, addr strCrlf
+	.elseif dChoice == 3
+
+	.elseif dChoice == 4
+
+	.elseif dChoice == 5
+
+	.elseif dChoice == 6
+
+	.elseif dChoice == 7
+
+	.elseif dChoice == 8
+
+	.elseif dChoice == 9
+	
+	.elseif dChoice == 10
+
+	.elseif dChoice == 11
+
+	.elseif dChoice == 12
+
+	.elseif dChoice == 13
+
+	.elseif dChoice == 14
+
+	.elseif dChoice == 15
+
+	.elseif dChoice == 16
+
+	.elseif dChoice == 17
+
+	.elseif dChoice == 18
+
+	.elseif dChoice == 19
+
+	.elseif dChoice == 20
+
+	.elseif dChoice == 21
+
+	.elseif dChoice == 22
+
+	.elseif dChoice == 23
+
+	.else
+
+	.endif
+	ret
+Input endp
+
 
 _main:
-	call Menu
+	.while dChoice != 23
+		call Menu
+		call Input
+	.endw
 
 	invoke ExitProcess, 0
 end _main
